f577c718e69fa6d65419135a10678f4e3a108ff8
ThatRandomGamer1337 <113189409+ThatRandomGamer1337@users.noreply.github.com>
2022-10-11 10:07:57 -0300

    Updates

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -17,33 +17,49 @@ namespace Conditionals
             Console.WriteLine("Conditionals");
             Console.WriteLine();
             Console.WriteLine();
-         
+
+            int enemyPoints;
+            int doublePoints;
+
             score = 0;
             health = 100;
+            enemyPoints = 25;
+            doublePoints = 2;
 
 
             // - - - - - - - - - - - - - - - - -
 
+            selectedWeapon(1);
             TakeDamage(0);
             ShowHUD();
 
             // Simulated Gameplay
 
+            selectedWeapon(1);
+            scoreCounting(enemyPoints, 1);
             TakeDamage(10);
             ShowHUD();
 
-
+            selectedWeapon(2);
+            scoreCounting(enemyPoints, 1);
             TakeDamage(20);
             ShowHUD();
 
+            selectedWeapon(5);
+            scoreCounting(enemyPoints, doublePoints);
             TakeDamage(30);
             ShowHUD();
 
+            selectedWeapon(3);
+            scoreCounting(enemyPoints, doublePoints);
             TakeDamage(20);
             ShowHUD();
 
+            selectedWeapon(4);
+            scoreCounting(0, 1);
             TakeDamage(50);
             ShowHUD();
+
 
             Console.ReadKey();
 
@@ -87,11 +103,54 @@ namespace Conditionals
 
 
         }
+
+        static void selectedWeapon(int weapon)
+        {
+
+            string gun;
+            
+            gun = ("Pistol");
+
+            if (weapon == 1)
+            {
+                gun = ("Pistol Equipped.");
+            }
+
+            if (weapon == 2)
+            {
+                gun = ("Rifle Equipped.");
+            }
+
+            if (weapon == 3)
+            {
+                gun = ("Shotgun Equipped.");
+            }
+
+            if (weapon == 4)
+            {
+                gun = ("RPG Equipped.");
+            }
+
+            if (weapon == 5)
+            {
+                gun = ("Sniper Equipped.");
+            }
+
+            Console.WriteLine("Weapon: " + gun);
+
+        }
+
+        static void scoreCounting(int points, int bonus)
+        {
+
+            score = score + points;
+            score = score * bonus;
+
+        }
         static void ShowHUD()
         {
             Console.WriteLine("Score: " + score);
             Console.WriteLine("Health: " + health);
-            Console.WriteLine();
             Console.WriteLine();
         }
     }     
